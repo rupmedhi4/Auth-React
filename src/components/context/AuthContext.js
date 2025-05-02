f17110fb34295bb949592de67388b23ceb00f6a0
Rup medhi <rupmmedhi@gmail.com>
2025-05-02 14:24:06 +0530

    auto logout

--- src/components/context/AuthContext.js
+++ src/components/context/AuthContext.js
@@ -1,33 +1,48 @@
-import { createContext, useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 
-const AuthContext = createContext();
+const AuthContext = React.createContext();
 
-export const AuthContextProvider = ({ children }) => {
-  const initialToken = localStorage.getItem('token');
-  const [token, setToken] = useState(initialToken);
+let logoutTimer;
 
-  const isLoggedIn = !!token;
+export const AuthContextProvider = (props) => {
+  const [token, setToken] = useState(null);
+
+  const userIsLoggedIn = !!token;
+
+  const logoutHandler = useCallback(() => {
+    setToken(null);
+    localStorage.removeItem('token');
+    if (logoutTimer) {
+      clearTimeout(logoutTimer);
+    }
+  }, []);
 
   const loginHandler = (token) => {
     setToken(token);
     localStorage.setItem('token', token);
+
+    logoutTimer = setTimeout(logoutHandler, 30 * 60 * 1000); 
   };
 
-  const logoutHandler = () => {
-    setToken(null);
-    localStorage.removeItem('token');
-  };
+  useEffect(() => {
+    const storedToken = localStorage.getItem('token');
+    if (storedToken) {
+      setToken(storedToken);
+
+      logoutTimer = setTimeout(logoutHandler, 30 * 60 * 1000);
+    }
+  }, [logoutHandler]);
 
   const contextValue = {
-    token,
-    isLoggedIn,
+    token: token,
+    isLoggedIn: userIsLoggedIn,
     login: loginHandler,
-    logout: logoutHandler
+    logout: logoutHandler,
   };
 
   return (
     <AuthContext.Provider value={contextValue}>
-      {children}
+      {props.children}
     </AuthContext.Provider>
   );
 };
